bd72d8769530528ab34d1cd2c43049acfa0dd478
Owaiz Ahammed <110183441+Owaizahmd01@users.noreply.github.com>
2022-07-29 09:45:18 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float bs, dn, hr , grs ;
+
+    printf("\n Enter the number: ");
+    scanf("%f", &bs);
+    dn = bs*0.4;
+    hr = dn*0.2;
+    grs = dn+hr+bs;
+
+    printf("\n Gross Salary: %0.2f", grs);
+    printf("\n dearness allowance: %0.2f", dn);
+    printf("\n house rent: %0.2f",hr);
+    printf("\n Basic Salary: %0.2f", bs);
+
+    return 0;
+
+}
