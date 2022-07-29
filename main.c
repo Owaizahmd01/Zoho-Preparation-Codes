71067311de29000650e707e7d5547eb59e0a2a09
Owaiz Ahammed <110183441+Owaizahmd01@users.noreply.github.com>
2022-07-29 09:39:01 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h> // standrad input output header
+
+int main()
+{
+     int a,b,c;
+
+      printf("Enter the number :  ");
+
+      scanf ("%d %d", &a,&b);
+
+      c= a+b;
+
+       printf("\nThe number is: %d ", c);
+
+      c = a-b;
+       printf ("\n Subtract: %d",c);
+
+
+       c= a*b;
+       printf("\nMultiply: %d",c);
+
+       c=a/b;
+       printf("\n Divide: %d",c);
+
+       c=a%b;
+       printf("\nModulus: %d",c);
+
+        return 0 ;
+}
